653fdf83bfa59a271db4966c7b9f16c355ee0c84
king <471897134@qq.com>
2018-12-16 13:45:39 +0800

    feat(deploy): add slogn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use( async(ctx) => {
-  ctx.body = 'hello world my name is kingpjchen';
+  ctx.body = 'hello world my name is kingpjchen done';
 });
 
 app.listen(8080);
